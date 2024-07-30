b7bc263ea6eb42d3e39f13b92f58a498984928ff
구홍석 <hsgpublic@gmail.com>
2024-07-30 17:00:52 +0900

    프로젝트 생성.

---
+++ NewsAPI/NewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPIApp.swift
+//  NewsAPI
+//
+//  Created by 구홍석 on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
