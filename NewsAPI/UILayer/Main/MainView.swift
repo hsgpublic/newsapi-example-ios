a2a29ad84495f2aa7560ae8a8c0706b304ce1639
구홍석 <hsgpublic@gmail.com>
2024-07-31 09:05:16 +0900

    SwiftUI에서 scene으로 변경.

---
+++ NewsAPI/UILayer/Main/MainView.swift
@@ -0,0 +1,8 @@
+//
+//  MainView.swift
+//  NewsAPI
+//
+//  Created by 구홍석 on 7/31/24.
+//
+
+import Foundation
---
+++ NewsAPI/UILayer/Main/MainViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewController.swift
+//  NewsAPI
+//
+//  Created by 구홍석 on 7/31/24.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+}
